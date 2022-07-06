e6758b2936ce73aa9b1e5b060bd07b307a294170
Amaobi Obikobe <obikobea@accessbankplc.com>
2022-07-06 04:58:08 +0100

    add error boundary

---
+++ src/pages/ResumePage/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import BlogPost from '../../components/BlogPost';
+import RootPage from '../RootPage';
+
+const LandingPage: FC = () => {
+  return (
+    <RootPage>
+      <BlogPost></BlogPost>
+    </RootPage>
+  );
+};
+
+export default LandingPage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import React, { FC } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from '../components/ErrorBoundary';
 import LandingPage from '../pages/LandingPage';
+import ResumePage from '../pages/ResumePage';
 
 const AllRoutes: FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/resume' element={<ResumePage />} />
+        </Routes>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 };
